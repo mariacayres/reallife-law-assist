48a0503b789c9b8d8590977fc116b5f908ec104d
f0777 <eleazer.mota@gmail.com>
2026-01-29 15:18:41 +0000

    feature/14:Done

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿﻿using System;
 using System.Collections.Generic;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 using RealLifeLawAssist.Configuration;
+using RealLifeLawAssist.Models;
 using RealLifeLawAssist.Services;
 
 try
